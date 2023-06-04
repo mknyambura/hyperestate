6b2f14e9e6bbbbf4b32b5865bb4a9714f665a8b9
mknyambura <kariuki.mercie@gmail.com>
2023-06-04 07:46:50 +0300

    crete header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='flex flex-rowtext-white'>
-        <h1 className='font-bold text-2xl uppercase'>Hypeesrate</h1>
+    <div className='flex flex-row justify-between text-white px-24 py-24'>
+        <h1 className='font-bold text-2xl uppercase'>Hyperesrate</h1>
         <div className='flex flex-row gap-8 font-extralight text-sm'>
-            <h1>Property</h1>
-            <h1>Services</h1>
-            <h1>Product</h1>
-            <h1>Aboyt Us</h1>
+            <h1 className='hover:underline hover:cursor-pointer'>Property</h1>
+            <h1 className='hover:underline hover:cursor-pointer'>Services</h1>
+            <h1 className='hover:underline hover:cursor-pointer'>Product</h1>
+            <h1 className='hover:underline hover:cursor-pointer'>About Us</h1>
         </div>
         <button className='bg-white rounded-none px-5 py-2 font-bold'>Contact</button>
     </div>
