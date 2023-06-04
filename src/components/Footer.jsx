0631d1fc418034f5972f39c0f83ee8ea4ffede8a
mknyambura <kariuki.mercie@gmail.com>
2023-06-04 08:01:01 +0300

    create newsletter section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='text-white flex flex-col bg-black px-24 py-24 gap-8'>
+    <div className='text-white flex flex-col bg-black px-24 py-12 gap-8'>
         <div className='bg-slate-400 h-[1px] mt-16 mb-16'></div>
         <div className='flex flex-row justify-between'>
             <h1 className='font-bold text-2xl uppercase'>Hyperestate</h1>
-            <div className='flex flex-row'>
-                <h1 className='hover:underline text-[15px] font-extralight hover:cursor-pointer text-white/70 hover:text-white'>Home</h1>
-                <h1 className='hover:underline text-[15px] font-extralight hover:cursor-pointer text-white/70 hover:text-white'>About</h1>
-                <h1 className='hover:underline text-[15px] font-extralight hover:cursor-pointer text-white/70 hover:text-white'>Service</h1>
-                <h1 className='hover:underline text-[15px] font-extralight hover:cursor-pointer text-white/70 hover:text-white'>Portfolio</h1>
-                <h1 className='hover:underline text-[15px] font-extralight hover:cursor-pointer text-white/70 hover:text-white'>Blog</h1>
-                <h1 className='hover:underline text-[15px] font-extralight hover:cursor-pointer text-white/70 hover:text-white'>Contact</h1>
+            <div className='flex flex-row gap-8'>
+                <h1 className='hover:underline text-[15px] font-extralight hover:cursor-pointer text-white/70 hover:text-white transition-all duration-500'>Home</h1>
+                <h1 className='hover:underline text-[15px] font-extralight hover:cursor-pointer text-white/70 hover:text-white transition-all duration-500'>About</h1>
+                <h1 className='hover:underline text-[15px] font-extralight hover:cursor-pointer text-white/70 hover:text-white transition-all duration-500'>Service</h1>
+                <h1 className='hover:underline text-[15px] font-extralight hover:cursor-pointer text-white/70 hover:text-white transition-all duration-500'>Portfolio</h1>
+                <h1 className='hover:underline text-[15px] font-extralight hover:cursor-pointer text-white/70 hover:text-white transition-all duration-500'>Blog</h1>
+                <h1 className='hover:underline text-[15px] font-extralight hover:cursor-pointer text-white/70 hover:text-white transition-all duration-500'>Contact</h1>
             </div>
         </div>
         <div className='flex items-center justify-center'>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,16 @@ function Home() {
         <div>
             <Header/>
             <div>
-                Home
+                <div className='bg-black flex flex-col gap-8 items-center text-white py-24'>
+                    <h1 className='font-bold text-5xl'>Ready to get started?</h1>
+                    <h1 className='font-semibold opacity-70'>To Join the worldwide community</h1>
+                    <div className='relative flex items-center'>
+                        <input type="email" className='text-black px-5 py-3 w-[500px] border-none outline-none ring-offset-transparent focus:ring-transparent focus:ring-offset-transparent rounded-md bg-white' placeholder='Enter Your Email'/>
+                        <div className='absolute right-1'>
+                            <button className='px-5 py-2 bg-[#FF3F3F] hover:bg-black transition-all duration-500 font-bold rounded-md'>Join Now</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <Footer/>
